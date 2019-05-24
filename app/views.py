1a44bc354a7ec9015ab0e1fd83b21ebfccd9ef80
dhdc1 <dhdctraining@gmail.com>
2019-05-24 15:14:58 +0700

    tehnn

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from . import db
+from .models import Comment
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def index():
+    comments = Comment.query.all()
+    return render_template('index.html', comments=comments)
+
+
+@main.route('/sign', methods=['GET', 'POST'])
+def sign():
+    if request.method == 'GET':
+        return render_template('sign.html')
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        comment = request.form.get('comment')
+
+        new_comment = Comment(name=name, comment_text=comment)
+        db.session.add(new_comment)
+        db.session.commit()
+
+        return redirect(url_for('main.index'))
